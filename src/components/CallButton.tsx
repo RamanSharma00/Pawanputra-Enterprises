d21716fc1c7499589989e4d17b48b8e88baef5e2
RamanSharma00 <ramanpipla32@gmail.com>
2026-07-23 22:47:43 +0530

    Adjusted call button spacing

--- src/components/CallButton.tsx
+++ src/components/CallButton.tsx
@@ -2,7 +2,7 @@ export default function CallButton() {
   return (
     <a
       href="tel:+918946955587"
-      className="fixed bottom-16 right-6 bg-blue-600 text-white w-14 h-14 rounded-full shadow-lg z-50 flex items-center justify-center hover:scale-110 transition-all duration-300"
+      className="fixed bottom-28 right-6 bg-blue-600 text-white w-14 h-14 rounded-full shadow-lg z-50 flex items-center justify-center hover:scale-110 transition-all duration-300"
     >
       📞
     </a>
